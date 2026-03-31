5bfedee5980b35ebd2ea8145ddc8ca6f57148086
Emilly Da Silva <emilly.nicacio@escolar.ifrn.edu.br>
2026-03-31 10:21:11 -0300

    Aula 31/03

--- AulaPlataforma/Assets/Scripts/AudioManager.cs
+++ AulaPlataforma/Assets/Scripts/AudioManager.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
+    private AudioSource systemSource;
+    private List<AudioManager> activeSoucers;
+
     #region Singleton
     public static AudioManager Instance;
 
@@ -12,12 +17,46 @@ public class AudioManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            systemSource = GetComponent<AudioSource>();
+            activeSoucers = new List<AudioManager>();
         }
         else
         {
             Destroy(gameObject);
         }
     }
+    
+    #endregion
+
+    #region AudioControls
+
+    public void Play(AudioClip clip)
+    {
+        systemSource.Stop();
+        systemSource.clip = clip;
+        systemSource.Play();
+    }
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        systemSource.PlayOneShot(clip);
+    }
 
     #endregion
+    
+    public void Play(AudioClip clip, AudioSource source)
+    {
+        if(!activeSoucers.Contains(source))
+            activeSoucers.Add(source);
+        source.Stop();
+        source.clip = clip;
+        source.Play();
+    }
+
+    public void Stop (AudioClip clip)
+    {
+        if(activeSoucers.Contains(source)
+           source.Stop();
+        activeSoucercs(clip);
+    }
 }
